a5a4be84f81bc6775938162029b483398669efa6
Serge <sergei.oussov@yandex.ru>
2023-04-03 16:48:50 +0300

    Решение задачи 2.2.1.5

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -2,8 +2,11 @@ package koschei.models;
 
 public class Egg6 {
 
+    private final Needle7 needl;
+    public Egg6(Needle7 needl) {this.needl = needl;}
+
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needl.toString();
     }
 }
